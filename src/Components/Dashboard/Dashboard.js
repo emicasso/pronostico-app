c0926eda4982b131c2bad06ad531b0625749f1a7
Emiliano Cassoni <cassoniemiliano@gmail.com>
2022-06-01 11:10:26 -0300

    fix height

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -6,7 +6,7 @@ export default function Dashboard() {
   return (
     <div className="bg-[#1F293B] flex items-center justify-center  py-10 font-Karla">
       <div className="container">
-      <div className="flex flex-col lg:flex-row w-4/3 min-h-[40rem] rounded-3xl shadow-lg lg:mx-auto mx-4 bg-[#E2EAF3] ">
+      <div className="flex flex-col lg:flex-row w-4/3 min-h-[50.8rem] rounded-3xl shadow-lg lg:mx-auto mx-4 bg-[#E2EAF3] ">
         <FormClima />
         <InfoClima />
       </div>
